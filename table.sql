709ff4df21e4f06c92d0bf9103238daec617d257
Ankit kumar <ankitkhadria1@gmail.com>
2014-12-09 13:31:39 +0530

    login with google plus
    Go to www.console.developer.google.com create your client id and replace
    in index.php

---
+++ table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `google_users` (
+	
+  `google_id` decimal(21,0)  NOT NULL AUTO_INCREAMENT ,
+  `google_name` varchar(60) NOT NULL,
+  `google_email` varchar(60) NOT NULL,
+  `google_link` varchar(60) NOT NULL,
+  `google_picture_link` varchar(60) NOT NULL,
+  PRIMARY KEY (`google_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
